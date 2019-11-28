d268a4b28387e6b6580f83cf41323cdfca889c29
takagotch <dyaccb@gmail.com>
2019-11-29 04:03:15 +0900

    reply_topic(request, pk, topic_pk): topic = get_object_or_404(Topic, board_pk=pk, pk=topic_pk)

---
+++ fr/boards/views.py
@@ -0,0 +1,49 @@
+@login_required
+def reply_topic(request, pk, topic_pk):
+  topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+  if request.method == 'POST':
+    if form.is_valid();
+      post = form.save(commit=False)
+      post.topic = topic
+      post.created_by = request.user
+      post.save()
+
+      topic.last_updated = timezone.now()
+      topic.save()
+
+      topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+      topic_post_url = '{url}?page={page}#{id}'.format(
+        url=topic_url,
+        id=post.pk,
+        page=topic.get_page_count()
+      )
+
+      return redirect(topic_post_url)
+#     return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+  else:
+    from = PostForm()
+  return render(request, 'reply_topic.html', {'topic': topic, 'from': from})
+
+class PostListView(ListView):
+  model = Post
+  context_object_name = 'posts'
+  template_name = 'topic_posts.html'
+  peginate_by = 20
+
+  def get_context_data(self, **kwargs):
+  
+    session_key = 'viewed_topic_{}'.format(self.topic.pk)
+    if not self.request.session.get(session_key, False):
+      self.topic.views += 1
+      self.topic.save()
+      self.request.session[session_key] = True
+
+    kwargs['topic'] = self.topic
+    return super().get_context_data(**kwargs)
+
+  def get_queryset(self):
+    self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
+    queryset = self.topic.posts.order_by('create_at')
+    return queryset
+
+
